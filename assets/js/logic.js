bf18f7cdb15a76e771b6e91bd97e37774ec19ccf
Girlyn Antillon <casey.girlyn@gmail.com>
2024-01-14 00:08:27 +0000

    add placeholder question and answers

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,13 +1,25 @@
-let btnStart = document.querySelector('#start-screen');
+let btnStartScreen = document.querySelector('#start-screen');
 let timerEl = document.querySelector('#time');
+let questionEl = document.querySelector('#questions');
+let questionTitle = document.querySelector('#question-title');
+let option1 = document.querySelector('#option1');
+let option2 = document.querySelector('#option2');
+let option3 = document.querySelector('#option3');
+let option4 = document.querySelector('#option4');
 
 let counter = 75;
+let questionCounter = 0;
 
-btnStart.addEventListener('click', function (event) {
+btnStartScreen.addEventListener('click', function (event) {
+    event.preventDefault();
     timerEl.textContent = counter;
+    questionEl.removeAttribute('class','hide');
+    btnStartScreen.setAttribute('class','hide');
+    loadQuestion();
     setInterval(function () {
         if (counter > 0) {
             counter--;
+            // Display countdown timer on the screen
             timerEl.textContent = counter;
         } else {
             return;
@@ -15,3 +27,20 @@ btnStart.addEventListener('click', function (event) {
     }, 1000);
 
 });
+
+
+function loadQuestion(){
+    let question = questions[questionCounter].question;
+    let ans1 = questions[questionCounter].option1;
+    let ans2 = questions[questionCounter].option2;
+    let ans3 = questions[questionCounter].option3;
+    let ans4 = questions[questionCounter].option4;
+
+    questionTitle.textContent = question;
+    option1.textContent = ans1;
+    option2.textContent = ans2;
+    option3.textContent = ans3;
+    option4.textContent = ans4;
+    questionCounter++;
+
+}
---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+let questions = [
+    {
+        question: "Q1",
+        option1: "1a",
+        option2: "1b",
+        option3: "1c",
+        option4: "1d",
+        correctAnswer: "a"
+    },
+    {
+        question: "Q2",
+        option1: "2a",
+        option2: "2b",
+        option3: "2c",
+        option4: "2d",
+        correctAnswer: "b"
+    },
+    {
+        question: "Q3",
+        option1: "3a",
+        option2: "3b",
+        option3: "3c",
+        option4: "3d",
+        correctAnswer: "c"
+    },
+    {
+        question: "Q4",
+        option1: "4a",
+        option2: "4b",
+        option3: "4c",
+        option4: "4d",
+        correctAnswer: "d"
+    },
+    {
+        question: "Q5",
+        option1: "5a",
+        option2: "5b",
+        option3: "5c",
+        option4: "5d",
+        correctAnswer: "d"
+    }
+    ,
+    {
+        question: "Q6",
+        option1: "6a",
+        option2: "6b",
+        option3: "6c",
+        option4: "6d",
+        correctAnswer: "a"
+    }
+];
+
