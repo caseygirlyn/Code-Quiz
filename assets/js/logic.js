fd24f3496e70e2d58cae72c2d5f105f668db0e30
Girlyn Antillon <casey.girlyn@gmail.com>
2024-01-14 22:03:24 +0000

    update questions and logic to load QAs

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,45 +2,103 @@ let btnStartScreen = document.querySelector('#start-screen');
 let timerEl = document.querySelector('#time');
 let questionEl = document.querySelector('#questions');
 let questionTitle = document.querySelector('#question-title');
-let option1 = document.querySelector('#option1');
-let option2 = document.querySelector('#option2');
-let option3 = document.querySelector('#option3');
-let option4 = document.querySelector('#option4');
+let choices = document.querySelector('#choices');
+let feedback = document.querySelector('#feedback');
+let btnchoice = document.querySelector('button');
+let finalScore = document.querySelector('#final-score');
+let endScreen = document.querySelector('#end-screen');
 
-let counter = 75;
+let countdown = 75;
 let questionCounter = 0;
+let correctAns = '';
+let totalScore = 0;
+var myTimer;
 
 btnStartScreen.addEventListener('click', function (event) {
-    event.preventDefault();
-    timerEl.textContent = counter;
-    questionEl.removeAttribute('class','hide');
-    btnStartScreen.setAttribute('class','hide');
-    loadQuestion();
-    setInterval(function () {
-        if (counter > 0) {
-            counter--;
-            // Display countdown timer on the screen
-            timerEl.textContent = counter;
-        } else {
-            return;
-        }
-    }, 1000);
-
+    event.preventDefault()
+    timerEl.textContent = countdown;
+    questionEl.classList.remove('hide');;
+    btnStartScreen.classList.add('hide');
+    loadQA();
+    myTimer = startTimer();
 });
 
+function startTimer() {
+    setInterval(function () {
+        if (countdown > 0) {
+            countdown--;
+            // Display countdown timer on the screen
+            timerEl.textContent = countdown;
+        } else {
+            timeIsUp()
+        }
+    }, 1000);
+}
 
-function loadQuestion(){
-    let question = questions[questionCounter].question;
-    let ans1 = questions[questionCounter].option1;
-    let ans2 = questions[questionCounter].option2;
-    let ans3 = questions[questionCounter].option3;
-    let ans4 = questions[questionCounter].option4;
+function timeIsUp() {
+    clearInterval(myTimer);
+    questionEl.classList.add('hide');
+    endScreen.classList.remove('hide');
+    feedback.classList.add('hide');
+    finalScore.innerHTML = totalScore;
+}
 
-    questionTitle.textContent = question;
-    option1.textContent = ans1;
-    option2.textContent = ans2;
-    option3.textContent = ans3;
-    option4.textContent = ans4;
-    questionCounter++;
+function validateAnswer(userAnswer) {
+    feedback.classList.remove('hide');
+    correctAns = questions[questionCounter - 1].correctAnswer;
+
+    if (userAnswer === correctAns) {
+        feedback.innerHTML = "Correct!";
+        totalScore++;
+    } else {
+        feedback.innerHTML = "Wrong!";
+    }
+    setTimeout(function () {
+        loadQA();
+    }, 1000);
+}
+
+function loadQA() {
+    if (choices.hasChildNodes()) {
+        choices.innerHTML = '';
+    }
+    if (questionCounter < questions.length) {
+        feedback.innerHTML = '';
+        feedback.classList.add('hide');
+        let question = questions[questionCounter].question;
+        let choice1 = questions[questionCounter].choice1;
+        let choice2 = questions[questionCounter].choice2;
+        let choice3 = questions[questionCounter].choice3;
+        let choice4 = questions[questionCounter].choice4;
+
+        questionTitle.textContent = question;
+
+        questionCounter++;
+
+        let buttonEl1 = document.createElement('button');
+        buttonEl1.setAttribute('onclick', 'validateAnswer(1)');
+
+        let buttonEl2 = document.createElement('button');
+        buttonEl2.setAttribute('onclick', 'validateAnswer(2)');
+
+        let buttonEl3 = document.createElement('button');
+        buttonEl3.setAttribute('onclick', 'validateAnswer(3)');
+
+        let buttonEl4 = document.createElement('button');
+        buttonEl4.setAttribute('onclick', 'validateAnswer(4)');
+
+        buttonEl1.textContent = '1. ' + choice1;
+        buttonEl2.textContent = '2. ' + choice2;
+        buttonEl3.textContent = '3. ' + choice3;
+        buttonEl4.textContent = '4. ' + choice4;
+
+        choices.appendChild(buttonEl1);
+        choices.appendChild(buttonEl2);
+        choices.appendChild(buttonEl3);
+        choices.appendChild(buttonEl4);
+
+    } else if (questionCounter === questions.length) {
+        timeIsUp()
+    }
 
 }
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,52 +1,88 @@
-let questions = [
+const questions = [
     {
-        question: "Q1",
-        option1: "1a",
-        option2: "1b",
-        option3: "1c",
-        option4: "1d",
-        correctAnswer: "a"
+        question: "Javascript is an _______ language",
+        choice1: "Object-Oriented",
+        choice2: "Object-Based",
+        choice3: "Procedural",
+        choice4: "None of the above",
+        correctAnswer: 1
     },
     {
-        question: "Q2",
-        option1: "2a",
-        option2: "2b",
-        option3: "2c",
-        option4: "2d",
-        correctAnswer: "b"
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        choice1: "const",
+        choice2: "let",
+        choice3: "Both A and B",
+        choice4: "None of the above",
+        correctAnswer: 3
     },
     {
-        question: "Q3",
-        option1: "3a",
-        option2: "3b",
-        option3: "3c",
-        option4: "3d",
-        correctAnswer: "c"
+        question: "Which of the following are closures in Javascript?",
+        choice1: "Variables",
+        choice2: "Functions",
+        choice3: "Objects",
+        choice4: "All of the above",
+        correctAnswer: 4
     },
     {
-        question: "Q4",
-        option1: "4a",
-        option2: "4b",
-        option3: "4c",
-        option4: "4d",
-        correctAnswer: "d"
+        question: "Which of the following is not a Javascript framework?",
+        choice1: "Vue",
+        choice2: "React",
+        choice3: "Node",
+        choice4: "None of the above",
+        correctAnswer: 4
     },
     {
-        question: "Q5",
-        option1: "5a",
-        option2: "5b",
-        option3: "5c",
-        option4: "5d",
-        correctAnswer: "d"
+        question: "How to stop an interval timer in Javascript?",
+        choice1: "clearTimer",
+        choice2: "stopInterval",
+        choice3: "clearInterval",
+        choice4: "None of the above",
+        correctAnswer: 3
     }
     ,
     {
-        question: "Q6",
-        option1: "6a",
-        option2: "6b",
-        option3: "6c",
-        option4: "6d",
-        correctAnswer: "a"
+        question: "How do we write a comment in javascript?",
+        choice1: "/**/",
+        choice2: "//",
+        choice3: "#",
+        choice4: "Both A and B",
+        correctAnswer: 4
+    }
+    ,
+    {
+        question: "Which function is used to serialize an object into a JSON string in Javascript?",
+        choice1: "convert()",
+        choice2: "parse()",
+        choice3: "stringify()",
+        choice4: "None of the above",
+        correctAnswer: 3
+    }
+    ,
+    {
+        question: "What keyword is used to declare an asynchronous function in Javascript?",
+        choice1: "async",
+        choice2: "await",
+        choice3: "setTimeout",
+        choice4: "None of the above",
+        correctAnswer: 1
+    }
+    ,
+    {
+        question: "Which of the following is not a Logical Operator",
+        choice1: "!",
+        choice2: "&&",
+        choice3: "||",
+        choice4: "None of the above",
+        correctAnswer: 4
+    }
+    ,
+    {
+        question: "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+        choice1: "Undefined",
+        choice2: "Boolean",
+        choice3: "Integer",
+        choice4: "Object",
+        correctAnswer: 4
     }
 ];
 
